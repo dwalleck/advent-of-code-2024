1ce9e2c5b50f1baecc81fb8aefa5ff7f77de024a
Daryl Walleck <dwalleck@gmail.com>
2024-12-02 13:12:12 -0600

    Solved part 1 of DayTwo

--- src/DayTwo/Program.cs
+++ src/DayTwo/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+var lines = File.ReadLines(path)
+    .Select(line => line.Split([' ', '\t', '\n', '\r'], StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse))
+    .ToArray();
+
+
+// cleaned up version
+var safeReports = lines.Count(sequence =>
+{
+    var line = sequence.ToArray();
+    var sign = Math.Sign(line[0] - line[1]);
+    return Enumerable.Range(0, line.Length - 1)
+        .Select(i =>
+        {
+            var diff = line[i] - line[i + 1];
+            return Math.Sign(diff) == sign && Math.Abs(diff) is > 0 and <= 3;
+        })
+        .All(x => x);
+});
+Console.WriteLine(safeReports);
+
+
+// var path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+// var lines = File.ReadLines(path)
+//    .Select(line => line.Split([' ', '\t', '\n', '\r'], StringSplitOptions.RemoveEmptyEntries)
+//    .Select(int.Parse));
+
+// var safeReports = 0;
+// foreach (var line in lines)
+// {
+//     var lineArr = line.ToArray();
+//     int sign = Math.Sign(lineArr[0].CompareTo(lineArr[1]));
+//     var validPairs = new List<bool>();
+//     for (int i = 0; i < lineArr.Length - 1; i++)
+//     {
+//         int diff = lineArr[i] - lineArr[i + 1];
+//         int currentSign = Math.Sign(diff);
+//         int absDiff = Math.Abs(diff);
+//         validPairs.Add(currentSign == sign && 0 < absDiff && absDiff <= 3);
+//     }
+//     if (validPairs.TrueForAll(x => x))
+//     {
+//         safeReports++;
+//     }
+
+// }
+
+// Console.WriteLine(safeReports);
