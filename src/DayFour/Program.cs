e0ff758f5fdb571f862ec6449c4c82d8aff7c435
Daryl Walleck <dwalleck@gmail.com>
2024-12-04 12:47:56 -0600

    Start of day 4

---
+++ src/DayFour/Program.cs
@@ -0,0 +1,18 @@
+﻿var grid = File.ReadAllLines("input.txt")
+    .Select(line => line.ToList())
+    .ToList();
+
+Console.WriteLine("Done");
+
+
+class CharInGrid()
+{
+    public int North { get; set; }
+    public int Northeast { get; set; }
+    public int East { get; set; }
+    public int Southeast { get; set; }
+    public int South { get; set; }
+    public int Southwest { get; set; }
+    public int West { get; set; }
+    public int Northwest { get; set; }
+}
