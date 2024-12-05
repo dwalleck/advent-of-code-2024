a6af051343dacbb5217e5e2a13d80166cc859190
Daryl Walleck <dwalleck@gmail.com>
2024-12-04 18:00:51 -0600

    Fighting with day 4

--- src/DayFour/Program.cs
+++ src/DayFour/Program.cs
@@ -1,69 +1,129 @@
-﻿var grid = string.Join("", File.ReadAllLines("input.txt"));
+﻿using System.Numerics;
 
-//Console.WriteLine($"7 - {grid[7]}, 9 - {grid[9]}, - 10007 {grid[10007]}, - 10008 {grid[10008]}, - 10009 {grid[10009]}");
-// .Select(line => line.ToList())
-// .ToList();
+var grid = File.ReadAllLines("input.txt")
+    .Select(line => line.ToList())
+    .ToList();
+// Given the starting character, the next letter needs to be (x + 1), (x - 1), (y + 1), (y + 1, x + 1), or (y + 1, x - 1)
+var word = "XMAS";
+var xmasCount = 0;
+var letterCoordinates = new Dictionary<(int x, int y), char>();
+var xCoordinates = new List<(int x, int y)>();
 
+for (int y = 0; y < grid.Count; y++)
+{
+    for (int x = 0; x < grid[y].Count; x++)
+    {
+        if (grid[x][y] == 'X')
+        {
+            xCoordinates.Add((x, y));
+        }
+        else
+        {
+            letterCoordinates[(x, y)] = grid[x][y];
+        }
+    }
+}
+
+foreach (var xCoord in xCoordinates)
+{
+    if (letterCoordinates[(xCoord.x + 1, xCoord.y)] == 'M')
+    {
+
+    }
+}
+
+// What I want is a list of transformations I want to apply, then check all the transformations
+
+// BigInteger i = 0;
+
+
+// foreach (var letterX in letterCoordinates['X'])
+// {
+//     var m = new Coordinate
+//     {
+//         x = letterX.x - 1,
+//         y = letterX.y
+//     };
+//     var a = new Coordinate
+//     {
+//         x = letterX.x - 2,
+//         y = letterX.y
+//     };
+//     var s = new Coordinate
+//     {
+//         x = letterX.x - 3,
+//         y = letterX.y
+//     };
+//     if (letterCoordinates['M'].Contains(m) && letterCoordinates['A'].Contains(a) && letterCoordinates['S'].Contains(s))
+//     {
+//         xmasCount++;
+//     }
+// }
+
+Console.WriteLine(xmasCount);
 Console.WriteLine("Done");
+
+// for (int y = 0; y < grid.Count; y++)
+// {
+//     for (int x = 0; x < grid[y].Count; x++)
+//     {
+//         if (grid[x][y] == 'X')
+//         {
+//             // Try the different possibilities
+//             if (x < grid[y].Count - 3)
+//             {
+//                 if (grid[x + 1][y] == 'M' && grid[x + 2][y] == 'A' && grid[x + 3][y] == 'S')
+//                 {
+//                     xmasCount++;
+//                 }
+//             }
+
+//             if (x > 3)
+//             {
+//                 if (grid[x - 1][y] == 'M' && grid[x - 2][y] == 'A' && grid[x - 3][y] == 'S')
+//                 {
+//                     xmasCount++;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
 
 // 7 + 1, 7 - 1, 7 + 10007, 7 + 10008, 7 + 10006
 // int firstX = grid.IndexOf;
 // bool IsXmas = grid[firstX + 1] == 'M' && grid[firstX + 2] == 'A' && grid[firstX + 3] == 'S';
 // Console.WriteLine(IsXmas);
 
-var xLocations = new List<int>();
+// var xLocations = new List<int>();
 
-for (int i = 0; i < grid.Length - 3; i++)
-{
-    if (grid[i] == 'X')
-    {
-        xLocations.Add(i);
-    }
-}
-
-var xmasCount = 0;
-foreach (int xLocation in xLocations)
-{
-    if (grid[xLocation + 1] == 'M' && grid[xLocation + 2] == 'A' && grid[xLocation + 3] == 'S')
-    {
-        xmasCount++;
-    }
-
-    if (xLocation > 3 && grid[xLocation - 1] == 'M' && grid[xLocation - 2] == 'A' && grid[xLocation - 3] == 'S')
-    {
-        xmasCount++;
-    }
-}
-
-Console.WriteLine(xmasCount);
-
-
-// 7, 8, 9, 10
-// 7, 6, 5, 4
-
-
-
-
-// void isNextLetter(int startingPosition, int currentPosition, int letterPosition)
+// for (int i = 0; i < grid.Length - 3; i++)
 // {
-//     if (grid[currentPosition] == word[letterPosition])
+//     if (grid[i] == 'X')
 //     {
-//         isNextLetter(currentPosition + 1, letterPosition + 1);
-//         isNextLetter(position - 1, letterPosition + 1);
-//         isNextLetter(position + 10000, letterPosition + 1);
-//         isNextLetter(position + 1, letterPosition + 1);
+//         xLocations.Add(i);
+//     }
+// }
+
+// var xmasCount = 0;
+// foreach (int xLocation in xLocations)
+// {
+//     if (grid[xLocation + 1] == 'M' && grid[xLocation + 2] == 'A' && grid[xLocation + 3] == 'S')
+//     {
+//         xmasCount++;
+//     }
+
+//     if (xLocation > 3 && grid[xLocation - 1] == 'M' && grid[xLocation - 2] == 'A' && grid[xLocation - 3] == 'S')
+//     {
+//         xmasCount++;
 //     }
 // }
 
 
-class CharInGrid()
+record Coordinate()
 {
-    public int North { get; set; }
-    public int Northeast { get; set; }
-    public int East { get; set; }
-    public int Southeast { get; set; }
-    public int South { get; set; }
-    public int Southwest { get; set; }
-    public int West { get; set; }
-    public int Northwest { get; set; }
-}
+    public int x { get; set; }
+    public int y { get; set; }
+};
